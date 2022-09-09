02e18b270dafd7ff084fbdfb46ae7c37f009a452
Ralkhedher <Reemaalkhedher@gmail.com>
2022-09-09 22:10:52 +0300

    docker mods

--- bin/upload_docker.sh
+++ bin/upload_docker.sh
@@ -2,21 +2,21 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
-DOCKER_HUB_ID="gampie"
-DOCKER_REPOSITORY="hello-app"
-VERSION="1.000"
+DOCKER_HUB_ID="reemalkhedher"
+# DOCKER_REPOSITORY="hello-app"
+# VERSION="1.000"
 
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=${DOCKER_HUB_ID}/${DOCKER_REPOSITORY}
+dockerpath="reemalkhedher/capstone:v1.0"
 
 # Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login -u ${DOCKER_HUB_ID}
-docker tag ${DOCKER_REPOSITORY}:${VERSION} ${dockerpath}:${VERSION}
+docker tag capstone $dockerpath
 
 # Step 3:
 # Push image to a docker repository
-docker push ${dockerpath}:${VERSION}
+docker push ${dockerpath}
