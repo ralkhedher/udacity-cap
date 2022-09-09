a2b0206a5f9b856a19f18834b2cca18fc8aa12a9
Ralkhedher <Reemaalkhedher@gmail.com>
2022-09-09 22:14:34 +0300

    fix container port

--- bin/build_docker.sh
+++ bin/build_docker.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-CONTAINER_NAME="hello-app"
-VERSION=1.000
+# CONTAINER_NAME="hello-app"
+# VERSION=1.000
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag ${CONTAINER_NAME}:${VERSION} hello_app
+# docker build --tag ${CONTAINER_NAME}:${VERSION} hello_app
+docker build --tag capstone hello_app
+
